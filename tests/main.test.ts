916d7e0bae49303c3fc493f247cdb12592a149b9
cipollafrancesco <francesco.cipolla@chili.com>
2022-04-07 09:28:17 +0200

    Implemented neighbours horizontal check

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -1,17 +1,43 @@
 class Game {
-    constructor(matrix: Array<string>) {
+    private matrix: Array<string>;
 
+    constructor(matrix: Array<string>) {
+        this.matrix = matrix;
     }
 
     next(): Array<string> {
-        return ['.'];
+        const result = [];
+        for (let i = 0; i < this.matrix.length; i++) {
+            if (this.matrix[i] === "*") {
+                if (this.matrix[i - 1] === "*" && this.matrix[i + 1] === "*") {
+                    result.push("*");
+                } else {
+                    result.push(".");
+                }
+            } else {
+                result.push(this.matrix[i]);
+            }
+        }
+        return result;
     }
 }
 
-it("single live cell dies", () => {
-    const matrix: Array<string> = ['*']
+describe("Game of Life", () => {
 
-    const newMatrix: Array<string> = new Game(matrix).next()
+    it("single live cell dies", () => {
+        const matrix: Array<string> = ["*"];
 
-    expect(newMatrix).toEqual(['.'])
+        const newMatrix: Array<string> = new Game(matrix).next();
+
+        expect(newMatrix).toEqual(["."]);
+    });
+
+    it("live cell with two live neighbours lives", () => {
+        const matrix: Array<string> = ["*", "*", "*",];
+
+        const newMatrix: Array<string> = new Game(matrix).next();
+
+        expect(newMatrix[1]).toEqual("*");
+    });
+
 });
